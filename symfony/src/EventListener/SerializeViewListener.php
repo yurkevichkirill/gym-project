b0ac1a499085553387c5d2db877946fc0c194596
Yurkevich Kirill <123456789+odin@users.noreply.github.com>
2026-03-04 13:16:00 +0300

    Fixed Serialize Listener to return status

--- symfony/src/EventListener/SerializeViewListener.php
+++ symfony/src/EventListener/SerializeViewListener.php
@@ -21,22 +21,28 @@ final class SerializeViewListener
 
         if ($result instanceof OkResponse) {
             if(is_array($result->data)) {
-                $event->setResponse(new JsonResponse([
-                    'data' => $result->data,
-                    'meta' => [
-                        'pagination' => [
-                            'page' => $result->page,
-                            'limit' => $result->limit,
-                            'total' => $result->total,
-                            'pages' => (int) ceil($result->total / $result->limit),
+                $event->setResponse(new JsonResponse(
+                    [
+                        'data' => $result->data,
+                        'meta' => [
+                            'pagination' => [
+                                'page' => $result->page,
+                                'limit' => $result->limit,
+                                'total' => $result->total,
+                                'pages' => (int) ceil($result->total / $result->limit),
+                            ],
+                            'sort' => $result->sort,
                         ],
-                        'sort' => $result->sort,
-                    ]
-                ]));
+                    ],
+                    $result->status,
+                ));
             } else {
-                $event->setResponse(new JsonResponse([
-                    'data' => $result->data
-                ]));
+                $event->setResponse(new JsonResponse(
+                    [
+                        'data' => $result->data
+                    ],
+                    $result->status,
+                ));
             }
 
         }
