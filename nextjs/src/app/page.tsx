5e08c098c80778a367a3a9a1ca8b64c4fa2dcb5e
Yurkevich Kirill <123456789+odin@users.noreply.github.com>
2026-03-20 11:10:14 +0300

    End with public routes frontend

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,7 +1,6 @@
 import "@/app/globals.css"
 import Home from "@/scenes/home";
 import Memberships from "@/scenes/memberships"
-import OurClasses from "@/scenes/ourClasses";
 import ContactUs from "@/scenes/contactUs";
 import OurTrainers from "@/scenes/ourTrainers";
 import TrainingTypes from "@/scenes/trainingTypes";
