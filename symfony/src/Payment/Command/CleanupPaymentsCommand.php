85452003d91e7cf8f1b26d7f49c20e92ad54e1a8
Yurkevich Kirill <123456789+odin@users.noreply.github.com>
2026-04-14 10:15:56 +0300

    Added cron to cleanup all expired payments

---
+++ symfony/src/Payment/Command/CleanupPaymentsCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Payment\Command;
+
+use App\Payment\Service\PaymentService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+
+#[AsCommand(name: 'app:payments:cleanup')]
+class CleanupPaymentsCommand
+{
+    public function __construct(
+        private PaymentService $paymentService
+    )
+    {}
+
+    public function __invoke(): int
+    {
+        $this->paymentService->cancelExpiredPayments();
+
+        return Command::SUCCESS;
+    }
+}
