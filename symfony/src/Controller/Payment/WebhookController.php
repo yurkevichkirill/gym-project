b45153e2bf1bd6ef73d844fa0d12298787610231
Yurkevich Kirill <123456789+odin@users.noreply.github.com>
2026-05-16 17:31:53 +0300

    Updated stripe controllers

--- symfony/src/Controller/Payment/WebhookController.php
+++ symfony/src/Controller/Payment/WebhookController.php
@@ -5,33 +5,44 @@ declare(strict_types=1);
 namespace App\Controller\Payment;
 
 use App\Payment\Service\StripeWebhookService;
+use App\Response\DTO\ErrorResponseDTO;
 use App\Response\NoContentResponse;
+use Nelmio\ApiDocBundle\Attribute\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 use OpenApi\Attributes as OA;
 
 final class WebhookController extends AbstractController
 {
+    /**
+     * @throws BadRequestHttpException
+     */
     #[Route('/api/webhooks/stripe/', methods: ['POST'], format: 'json')]
     #[OA\Post(
         operationId: 'stripeWebhook',
         description: 'Endpoint for Stripe to send asynchronous event notifications (payment success, failure, etc.). Requires a valid stripe-signature header.',
         summary: 'Handle Stripe webhooks.',
         tags: ['Payment: Webhook'],
+        parameters: [
+            new OA\Parameter(
+                name: 'stripe-signature',
+                description: 'Stripe webhook signature for payload verification',
+                in: 'header',
+                required: true,
+                schema: new OA\Schema(type: 'string')
+            )
+        ],
         responses: [
             new OA\Response(
-                response: 200,
-                description: 'Webhook handled successfully',
-                content: new OA\JsonContent(
-                    properties: [
-                        new OA\Property(property: 'status', type: 'string', example: 'ok')
-                    ]
-                )
+                response: 204,
+                description: 'Webhook handled successfully'
             ),
             new OA\Response(
                 response: 400,
-                description: 'Bad Request - Invalid payload or signature verification failed.'
+                description: 'Bad Request (Invalid payload or signature verification failed)',
+                content: new OA\JsonContent(ref: new Model(type: ErrorResponseDTO::class))
             )
         ]
     )]
