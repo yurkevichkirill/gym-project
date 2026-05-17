adbfd7339cd5c9578996c0065d162915ecd4b9cb
Yurkevich Kirill <123456789+odin@users.noreply.github.com>
2026-05-17 12:23:27 +0300

    Setup phpstan

---
+++ symfony/tests/object-manager.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists(__DIR__ . '/bootstrap.php')) {
+    require __DIR__ . '/bootstrap.php';
+} elseif (class_exists(Dotenv::class) && file_exists(__DIR__.'/../.env')) {
+    new Dotenv()->bootEnv(__DIR__.'/../.env');
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'test', (bool) ($_SERVER['APP_DEBUG'] ?? true));
+$kernel->boot();
+
+return $kernel->getContainer()->get('doctrine')->getManager();
