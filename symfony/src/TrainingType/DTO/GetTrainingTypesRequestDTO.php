34cfbf65ddbe403065df7ea398145cad646b0889
Yurkevich Kirill <151058004+yurkevichkirill@users.noreply.github.com>
2026-06-23 17:07:21 +0300

    Bound training type list page size

--- symfony/src/TrainingType/DTO/GetTrainingTypesRequestDTO.php
+++ symfony/src/TrainingType/DTO/GetTrainingTypesRequestDTO.php
@@ -17,6 +17,7 @@ final readonly class GetTrainingTypesRequestDTO
         public int $page = 1,
 
         #[Assert\Positive]
+        #[Assert\LessThanOrEqual(100)]
         public int $limit = 20,
     )
     {}
