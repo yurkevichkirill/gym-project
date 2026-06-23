2d90dfba82da5c52e2478cc3973e4b9c79d73d8e
Yurkevich Kirill <151058004+yurkevichkirill@users.noreply.github.com>
2026-06-23 17:06:50 +0300

    Bound membership list page size

--- symfony/src/Membership/DTO/GetMembershipsRequestDTO.php
+++ symfony/src/Membership/DTO/GetMembershipsRequestDTO.php
@@ -34,6 +34,7 @@ final readonly class GetMembershipsRequestDTO
         public int $page = 1,
 
         #[Assert\Positive]
+        #[Assert\LessThanOrEqual(100)]
         public int $limit = 20,
     )
     {}
