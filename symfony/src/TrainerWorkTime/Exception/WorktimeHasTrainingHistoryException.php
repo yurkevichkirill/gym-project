905d50d6e4ef78113c0dfc29083e4f452eac60f3
Yurkevich Kirill <151058004+yurkevichkirill@users.noreply.github.com>
2026-06-26 13:36:01 +0300

    Add worktime training history exception

---
+++ symfony/src/TrainerWorkTime/Exception/WorktimeHasTrainingHistoryException.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\TrainerWorkTime\Exception;
+
+use DomainException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\WithHttpStatus;
+use Throwable;
+
+#[WithHttpStatus(Response::HTTP_CONFLICT)]
+final class WorktimeHasTrainingHistoryException extends DomainException
+{
+    public function __construct(
+        string $message = 'Trainer worktime has associated training history',
+        int $code = 0,
+        ?Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+}
